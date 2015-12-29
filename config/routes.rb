f1b6f579c1a3060f091cb9d2686f398b00cf71f1
Benjamin Hanna <benhanna@Benjamins-MacBook-Pro.local>
2015-12-29 13:49:55 +1100

    fix css

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount Payola::Engine => '/payola', as: :payola
   root to: 'visitors#index'
   devise_for :users
   resources :users
---
+++ db/migrate/20151228104717_addelementstoregistration.rb
@@ -0,0 +1,4 @@
+class Addelementstoregistration < ActiveRecord::Migration
+  def change
+  end
+end
