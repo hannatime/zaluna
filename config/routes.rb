087a716893d0f343105232cafc6b6275f25f078e
Benjamin Hanna <benhanna@Benjamins-MacBook-Pro.local>
2015-12-29 20:42:51 +1100

    Payola Gone

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  mount Payola::Engine => '/payola', as: :payola
   root to: 'visitors#index'
   devise_for :users
   resources :users
+  resources :subscriptions
   
 end
