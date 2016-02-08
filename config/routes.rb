9f18afe3a6b5e4ec3f25886d9e07042702415f40
Benjamin Hanna <benhanna@Benjamins-MacBook-Pro.local>
2016-02-08 21:45:54 +1100

    Through to IP issue

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   mount Koudoku::Engine, at: 'koudoku'
   scope module: 'koudoku' do
     get 'pricing' => 'subscriptions#index', as: 'pricing'
-    
+    get 'edit' => 'registration#edit', as: 'edit'
   end
 
 
@@ -12,7 +12,5 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users
   resources :charges
-
-
 
 end
